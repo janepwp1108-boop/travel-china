672305ba115ee148ca4f30d05f0c4c5c5e4ed1fa
janepwp1108-boop <janepwp1108@gmail.com>
2026-06-16 13:05:04 +0700

    update project

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hello word")
+
+function updateClock(){
+
+    const now = new Date();
+
+    // เพิ่ม 1 ชั่วโมง
+    now.setHours(now.getHours() + 1);
+
+    const time =
+        now.getHours().toString().padStart(2,'0')
+        + ":" +
+        now.getMinutes().toString().padStart(2,'0')
+        + ":" +
+        now.getSeconds().toString().padStart(2,'0');
+
+    document.getElementById("clock").innerHTML = time;
+}
+
+setInterval(updateClock,1000);
+updateClock();
+
+
